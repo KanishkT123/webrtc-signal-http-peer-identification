e0b8b24fc991ab87668fb4e091a5eacbe421fda8
Kanishk Tantia <t-katan@microsoft.com>
2018-06-04 14:01:52 -0400

    Fixed some problems with the original signaling server, used the new dataFor method in the extension

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 const express = require('express')
 const signalRouterCreator = require('webrtc-signal-http')
+const recognitionRouterCreator = require('./lib')
 
 const app = express()
-
 const recognitionRouter = recognitionRouterCreator()
+
 const signalRouter = signalRouterCreator({
     peerList: recognitionRouter.peerList,
     enableLogging: process.env.WEBRTC_SIGNAL_LOGGING || true,
-    enableCORS: true
+    enableCors: true
 })
 
-app.use(signalRouter, recognitionRouter)
-app.listen(process.env.PORT || 3000)
+app.use(signalRouter, recognitionRouter).listen(process.env.PORT || 3000)
